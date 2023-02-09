a2f275e01aa22c2d984a576d8d7ae8c17257141d
Wes <71149180+wesleymccollam@users.noreply.github.com>
2023-02-09 16:55:22 -0500

    access control handler resource (#21)
    * access control handler resource
    
    * switch test values
    
    * testing if test value doesn't change
    
    * removing test
    
    * Write some debug output, temporarily disable codeql
    
    * Enable debug output
    
    * Log warning info
    
    * Print ID output in read method
    
    * Try setting to false both times
    
    * Try not setting any properties
    
    * Revert "Try not setting any properties"
    
    This reverts commit b0dc7d3d21d0f8765aa4ff6dfed28d974cd64cb5.
    
    * Print PD logs after test failure
    
    * Don't run in parallel
    
    * Zee access control handler
    
    * Revert "Zee access control handler"
    
    This reverts commit bef298b01501b595d7d8ec6ca541b0d2ba6dc076.
    
    * Switch the right file name
    
    * Move file back and increase sizelimit
    
    * Enable debug trace logging
    
    * Fix docker command and run status
    
    * Fix log file paths
    
    * Limit logging
    
    * Print current time in some spots
    
    * Test skipping root dn test
    
    * Just comment out the root dn test
    
    * Avoid running in parallel if possible
    
    * Test with parallel 4
    
    * Test different parallel argument
    
    * Test different parallel argument
    
    * One other parallel argument
    
    * Undo testing changes and reduce permissions changes in root dn test
    
    * Small fixes
    
    ---------
    
    Co-authored-by: Henry Recker <henryrecker@pingidentity.com>

---
+++ examples/resources/pingdirectory_dsee_compat_access_control_handler/import.sh
@@ -0,0 +1,2 @@
+# "dseeCompatAccessControlHandlerName" should be the name of the Dsee Compat Access Control Handler to be imported
+terraform import pingdirectory_dsee_compat_access_control_handler dseeCompatAccessControlHandlerName
