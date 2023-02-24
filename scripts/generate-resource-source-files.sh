b21070c59b90862326b417076407613166326b8d
wesleymccollam <wesleymccollam@pingidentity.com>
2023-02-24 14:49:57 -0500

    existing container check, robust config failure

--- scripts/generate-resource-source-files.sh
+++ scripts/generate-resource-source-files.sh
@@ -9,26 +9,29 @@ fi
 
 echo "Generating resource files"
 
-# Start a PD container to read the config model from
-echo "Starting PingDirectory container to read config model from"
-docker run --name pingdirectory_terraform_generator \
-	-d -p 1389:1389 \
-	-e TAIL_LOG_FILES= \
-	--env-file "${HOME}/.pingidentity/config" \
-	"pingidentity/pingdirectory:${PINGDIRECTORY_TAG:-9.1.0.0-latest}"
+# Check (and start if needed) PD container to read the config model from
+if (docker ps | grep -E "pingdirectory_terraform_provider_container" >> /dev/null); then
+  echo "Existing PingDirectory Terraform container exists. No need to start a new one..."
+else
+  echo "No existing PingDirectory Terraform container. Starting a new one..."
+	docker run --name pingdirectory_terraform_provider_container \
+		-d -p 1389:1389 \
+		-e TAIL_LOG_FILES= \
+		--env-file "${HOME}/.pingidentity/config" \
+		"pingidentity/pingdirectory:${PINGDIRECTORY_TAG:-9.1.0.0-latest}"
+	# Wait for the instance to become ready, up to 4 minutes
+	echo "Waiting for PingDirectory to become ready..."
+	sleep 1
+	duration=0
+	while (( duration < 240 )) && ! docker logs pingdirectory_terraform_provider_container 2>&1 | grep -q "Setting Server to Available"; \
+	do \
+			duration=$((duration+1)); \
+		sleep 1; \
+	done
 
-# Wait for the instance to become ready, up to 4 minutes
-echo "Waiting for PingDirectory to become ready..."
-sleep 1
-duration=0
-while (( duration < 240 )) && ! docker logs pingdirectory_terraform_generator 2>&1 | grep -q "Setting Server to Available"; \
-do \
-    duration=$((duration+1)); \
-	sleep 1; \
-done
-
-# Fail if the container didn't become ready in time
-docker logs pingdirectory_terraform_generator 2>&1 | grep -q "Setting Server to Available"
+	# Fail if the container didn't become ready in time
+	docker logs pingdirectory_terraform_provider_container 2>&1 | grep -q "Setting Server to Available"
+fi
 
 # Run the generator, specifying the endpoints to be generated.
 # --endpoint can be specified multiple times to generate multiple endpoints in one run.
@@ -39,4 +42,4 @@ java -jar ./bin/pingdirectory-openapi-generator.jar \
 
 # Remove the PD container
 echo "Stopping and removing PingDirectory container"
-docker rm -f pingdirectory_terraform_generator
+docker rm -f pingdirectory_terraform_provider_container
