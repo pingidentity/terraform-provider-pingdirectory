c2fb0bd3b7a7131c29686fe4790eeebe1c1fd6e7
wesleymccollam <wesleymccollam@pingidentity.com>
2023-03-10 15:17:32 -0500

    git merge main and tools.go edit

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -3,12 +3,6 @@
 
 package tools
 
-//go:generate go install github.com/bflad/tfproviderlint/cmd/tfproviderlintx
-//go:generate go install github.com/golangci/golangci-lint/cmd/golangci-lint
-//go:generate go install github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
-//go:generate go install github.com/katbyte/terrafmt
-//go:generate go install github.com/terraform-linters/tflint
-
 import (
 	_ "github.com/bflad/tfproviderlint/cmd/tfproviderlintx"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
