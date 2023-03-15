1f75acff0530d39597dc4e3e30a4e4e40575594e
Henry Recker <henryrecker@pingidentity.com>
2023-03-15 12:01:26 -0500

    Handle new attribute in v9.2 for PingOne pass through authentication plugin

--- internal/resource/config/externalserver/amazon_aws_external_server_resource.go
+++ internal/resource/config/externalserver/amazon_aws_external_server_resource.go
@@ -150,15 +150,15 @@ func amazonAwsExternalServerSchema(ctx context.Context, req resource.SchemaReque
 
 // Validate that no unsupported attributes are being used by this resource
 func (r *defaultAmazonAwsExternalServerResource) ModifyPlan(ctx context.Context, req resource.ModifyPlanRequest, resp *resource.ModifyPlanResponse) {
-	CheckUnsupportedAttributes(r.providerConfig.PingDirectoryVersion, ctx, req, resp)
+	checkUnsupportedAttributesAmazonAwsExternalServer(r.providerConfig.PingDirectoryVersion, ctx, req, resp)
 }
 
 // Validate that no unsupported attributes are being used by this resource
 func (r *amazonAwsExternalServerResource) ModifyPlan(ctx context.Context, req resource.ModifyPlanRequest, resp *resource.ModifyPlanResponse) {
-	CheckUnsupportedAttributes(r.providerConfig.PingDirectoryVersion, ctx, req, resp)
+	checkUnsupportedAttributesAmazonAwsExternalServer(r.providerConfig.PingDirectoryVersion, ctx, req, resp)
 }
 
-func CheckUnsupportedAttributes(pdVersion string, ctx context.Context, req resource.ModifyPlanRequest, resp *resource.ModifyPlanResponse) {
+func checkUnsupportedAttributesAmazonAwsExternalServer(pdVersion string, ctx context.Context, req resource.ModifyPlanRequest, resp *resource.ModifyPlanResponse) {
 	// Check what version we are running
 	compare, err := version.Compare(pdVersion, version.PingDirectory9200)
 	if err != nil {
