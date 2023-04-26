ac299259f0c5ff760e61493b0999b748bab1e0cc
Henry Recker <henryrecker@pingidentity.com>
2023-04-26 10:10:28 -0500

    SCIM attribute mapping resource

---
+++ examples/resources/pingdirectory_scim_attribute_mapping/import.sh
@@ -0,0 +1,2 @@
+# Importing a Scim Attribute Mapping requires providing the name of all parent resources in the following format
+terraform import pingdirectory_scim_attribute_mapping.myScimAttributeMapping "[scim-resource-type-name]/[scim-attribute-mapping-name]"
