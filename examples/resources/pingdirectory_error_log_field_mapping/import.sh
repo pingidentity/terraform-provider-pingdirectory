c5da93f91c7b78b9f8c64c38b2186d09a8d954a6
David Ross <davidross@pingidentity.com>
2023-05-02 15:56:29 -0500

    Add log-field-mapping resource

---
+++ examples/resources/pingdirectory_error_log_field_mapping/import.sh
@@ -0,0 +1,2 @@
+# "errorLogFieldMappingId" should be the id of the Error Log Field Mapping to be imported
+terraform import pingdirectory_error_log_field_mapping.myErrorLogFieldMapping errorLogFieldMappingId
