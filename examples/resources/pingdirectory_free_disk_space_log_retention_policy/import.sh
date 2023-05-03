3a367e1da337b875418326c8c83a49c2ed4be1ba
PingDavidR <davidross@pingidentity.com>
2023-05-03 10:31:51 -0500

    Add log-retention-policy resource (#172)

---
+++ examples/resources/pingdirectory_free_disk_space_log_retention_policy/import.sh
@@ -0,0 +1,2 @@
+# "freeDiskSpaceLogRetentionPolicyId" should be the id of the Free Disk Space Log Retention Policy to be imported
+terraform import pingdirectory_free_disk_space_log_retention_policy.myFreeDiskSpaceLogRetentionPolicy freeDiskSpaceLogRetentionPolicyId
