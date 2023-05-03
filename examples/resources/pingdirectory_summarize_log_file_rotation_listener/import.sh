cd8f6ec75cf72280a6a83aa82e5d6d1384efa562
David Ross <davidross@pingidentity.com>
2023-05-03 09:25:52 -0500

    Add log-file-rotation-listener resource

---
+++ examples/resources/pingdirectory_summarize_log_file_rotation_listener/import.sh
@@ -0,0 +1,2 @@
+# "summarizeLogFileRotationListenerId" should be the id of the Summarize Log File Rotation Listener to be imported
+terraform import pingdirectory_summarize_log_file_rotation_listener.mySummarizeLogFileRotationListener summarizeLogFileRotationListenerId
