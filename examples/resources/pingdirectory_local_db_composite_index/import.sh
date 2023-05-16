b319b952eb3737f8919680ad698de81fab50d050
PingDavidR <davidross@pingidentity.com>
2023-05-16 12:28:11 -0500

    Add local-db-composite-index resource (#196)

---
+++ examples/resources/pingdirectory_local_db_composite_index/import.sh
@@ -0,0 +1,2 @@
+# Importing a Local Db Composite Index requires providing the name of all parent resources in the following format
+terraform import pingdirectory_local_db_composite_index.myLocalDbCompositeIndex "[backend-name]/[local-db-composite-index-name]"
