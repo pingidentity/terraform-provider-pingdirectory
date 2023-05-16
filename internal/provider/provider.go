c6a3ca71ab4ab0ece366f79b576220097ff9adbc
David Ross <davidross@pingidentity.com>
2023-05-16 12:06:52 -0500

    Add local-db-composite-index resource

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -467,6 +467,8 @@ func (p *pingdirectoryProvider) Resources(_ context.Context) []func() resource.R
 		config.NewLdapSdkDebugLoggerResource,
 		config.NewLocalDbIndexResource,
 		config.NewDefaultLocalDbIndexResource,
+		config.NewLocalDbCompositeIndexResource,
+		config.NewDefaultLocalDbCompositeIndexResource,
 		config.NewLocationResource,
 		config.NewRecurringTaskChainResource,
 		config.NewDefaultRecurringTaskChainResource,
