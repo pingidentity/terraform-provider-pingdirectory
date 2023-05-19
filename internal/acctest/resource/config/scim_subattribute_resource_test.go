2c20edfcb582057893136d263eb64a84da79ace4
Tim Nguyen <timothynguyen@pingidentity.com>
2023-05-19 14:56:14 -0500

    update test

--- internal/acctest/resource/config/scim_subattribute_resource_test.go
+++ internal/acctest/resource/config/scim_subattribute_resource_test.go
@@ -21,6 +21,7 @@ type scimSubattributeTestModel struct {
 	id                string
 	scimAttributeName string
 	scimSchemaName    string
+	caseExact         bool
 }
 
 func TestAccScimSubattribute(t *testing.T) {
@@ -29,6 +30,13 @@ func TestAccScimSubattribute(t *testing.T) {
 		id:                testIdScimSubattribute,
 		scimAttributeName: testScimAttributeName,
 		scimSchemaName:    testScimSchemaNametest,
+		caseExact:         true,
+	}
+	updatedResourceModel := scimSubattributeTestModel{
+		id:                testIdScimSubattribute,
+		scimAttributeName: testScimAttributeName,
+		scimSchemaName:    testScimSchemaNametest,
+		caseExact:         false,
 	}
 
 	resource.Test(t, resource.TestCase{
@@ -42,6 +50,12 @@ func TestAccScimSubattribute(t *testing.T) {
 				// Test basic resource.
 				// Add checks for computed properties here if desired.
 				Config: testAccScimSubattributeResource(resourceName, initialResourceModel),
+				Check:  testAccCheckExpectedScimSubattributeAttributes(initialResourceModel),
+			},
+			{
+				// Test updating some fields
+				Config: testAccScimSubattributeResource(resourceName, updatedResourceModel),
+				Check:  testAccCheckExpectedScimSubattributeAttributes(updatedResourceModel),
 			},
 			{
 				// Test importing the resource
@@ -73,10 +87,33 @@ resource "pingdirectory_scim_subattribute" "%[1]s" {
   id                  = "%[2]s"
   scim_attribute_name = pingdirectory_scim_attribute.%[3]s.name
   scim_schema_name    = pingdirectory_scim_schema.myScimSchema.schema_urn
+  case_exact          = %[5]t
 }`, resourceName,
 		resourceModel.id,
 		resourceModel.scimAttributeName,
-		resourceModel.scimSchemaName)
+		resourceModel.scimSchemaName,
+		resourceModel.caseExact)
+
+}
+
+// Test that the expected attributes are set on the PingDirectory server
+func testAccCheckExpectedScimSubattributeAttributes(config scimSubattributeTestModel) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		testClient := acctest.TestClient()
+		ctx := acctest.TestBasicAuthContext()
+		response, _, err := testClient.ScimSubattributeApi.GetScimSubattribute(ctx, config.id, config.scimAttributeName, config.scimSchemaName).Execute()
+		if err != nil {
+			return err
+		}
+		// Verify that attributes have expected values
+		resourceType := "SCIM Subattribute"
+		err = acctest.TestAttributesMatchBool(resourceType, &config.id, "case-exact",
+			config.caseExact, response.CaseExact)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
 }
 
 // Test that any objects created by the test are destroyed
