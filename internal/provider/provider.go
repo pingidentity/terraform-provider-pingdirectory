5957bc809041fb0fbc5396cb92e36b7756ed6729
Tim Nguyen <timothynguyen@pingidentity.com>
2023-05-26 10:30:02 -0500

    make fmt

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -501,7 +501,7 @@ func (p *pingdirectoryProvider) Resources(_ context.Context) []func() resource.R
 		config.NewDefaultScimSubattributeResource,
 		config.NewScimSubattributeResource,
 		config.NewDefaultServerGroupResource,
-        config.NewServerGroupResource,
+		config.NewServerGroupResource,
 		config.NewSoftDeletePolicyResource,
 		config.NewDefaultSoftDeletePolicyResource,
 		config.NewTopologyAdminUserResource,
