02d25a704bb34bdc5755e2a90e7e1f17798ac6e6
Henry Recker <henryrecker@pingidentity.com>
2023-06-14 10:49:52 -0500

    Rename test method

--- internal/acctest/resource/config/azureauthenticationmethod/azure_authentication_method_resource_test.go
+++ internal/acctest/resource/config/azureauthenticationmethod/azure_authentication_method_resource_test.go
@@ -12,17 +12,17 @@ import (
 	"github.com/pingidentity/terraform-provider-pingdirectory/internal/provider"
 )
 
-const testIdAzureAuthenticationMethod = "MyId"
+const testIdDefaultAzureAuthenticationMethod = "MyId"
 
 // Attributes to test with. Add optional properties to test here if desired.
-type azureAuthenticationMethodTestModel struct {
+type defaultAzureAuthenticationMethodTestModel struct {
 	id string
 }
 
-func TestAccAzureAuthenticationMethod(t *testing.T) {
+func TestAccDefaultAzureAuthenticationMethod(t *testing.T) {
 	resourceName := "myresource"
-	initialResourceModel := azureAuthenticationMethodTestModel{
-		id: testIdAzureAuthenticationMethod,
+	initialResourceModel := defaultAzureAuthenticationMethodTestModel{
+		id: testIdDefaultAzureAuthenticationMethod,
 	}
 
 	resource.Test(t, resource.TestCase{
@@ -30,16 +30,16 @@ func TestAccAzureAuthenticationMethod(t *testing.T) {
 		ProtoV6ProviderFactories: map[string]func() (tfprotov6.ProviderServer, error){
 			"pingdirectory": providerserver.NewProtocol6WithError(provider.New()),
 		},
-		CheckDestroy: testAccCheckAzureAuthenticationMethodDestroy,
+		CheckDestroy: testAccCheckDefaultAzureAuthenticationMethodDestroy,
 		Steps: []resource.TestStep{
 			{
 				// Test basic resource.
 				// Add checks for computed properties here if desired.
-				Config: testAccAzureAuthenticationMethodResource(resourceName, initialResourceModel),
+				Config: testAccDefaultAzureAuthenticationMethodResource(resourceName, initialResourceModel),
 			},
 			{
 				// Test importing the resource
-				Config:            testAccAzureAuthenticationMethodResource(resourceName, initialResourceModel),
+				Config:            testAccDefaultAzureAuthenticationMethodResource(resourceName, initialResourceModel),
 				ResourceName:      "pingdirectory_azure_authentication_method." + resourceName,
 				ImportStateId:     initialResourceModel.id,
 				ImportState:       true,
@@ -52,35 +52,22 @@ func TestAccAzureAuthenticationMethod(t *testing.T) {
 	})
 }
 
-func testAccAzureAuthenticationMethodResource(resourceName string, resourceModel azureAuthenticationMethodTestModel) string {
+func testAccDefaultAzureAuthenticationMethodResource(resourceName string, resourceModel defaultAzureAuthenticationMethodTestModel) string {
 	return fmt.Sprintf(`
 resource "pingdirectory_azure_authentication_method" "%[1]s" {
   type = "default"
-	 id = "%[2]s"
+  id   = "%[2]s"
 }`, resourceName,
 		resourceModel.id)
 }
 
-// Test that the expected attributes are set on the PingDirectory server
-func testAccCheckExpectedAzureAuthenticationMethodAttributes(config azureAuthenticationMethodTestModel) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		testClient := acctest.TestClient()
-		ctx := acctest.TestBasicAuthContext()
-		_, _, err := testClient.AzureAuthenticationMethodApi.GetAzureAuthenticationMethod(ctx, config.id).Execute()
-		if err != nil {
-			return err
-		}
-		return nil
-	}
-}
-
 // Test that any objects created by the test are destroyed
-func testAccCheckAzureAuthenticationMethodDestroy(s *terraform.State) error {
+func testAccCheckDefaultAzureAuthenticationMethodDestroy(s *terraform.State) error {
 	testClient := acctest.TestClient()
 	ctx := acctest.TestBasicAuthContext()
-	_, _, err := testClient.AzureAuthenticationMethodApi.GetAzureAuthenticationMethod(ctx, testIdAzureAuthenticationMethod).Execute()
+	_, _, err := testClient.AzureAuthenticationMethodApi.GetAzureAuthenticationMethod(ctx, testIdDefaultAzureAuthenticationMethod).Execute()
 	if err == nil {
-		return acctest.ExpectedDestroyError("Azure Authentication Method", testIdAzureAuthenticationMethod)
+		return acctest.ExpectedDestroyError("Default Azure Authentication Method", testIdDefaultAzureAuthenticationMethod)
 	}
 	return nil
 }
