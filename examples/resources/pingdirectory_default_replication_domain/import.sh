6164eb06378e6bbaaa53845817b1ba01807a6659
Henry Recker <henryrecker@pingidentity.com>
2023-06-16 11:52:21 -0500

    Replication domain resource

---
+++ examples/resources/pingdirectory_default_replication_domain/import.sh
@@ -0,0 +1,2 @@
+# Importing a Replication Domain requires providing the name of all parent resources in the following format
+terraform import pingdirectory_default_replication_domain.myReplicationDomain "[synchronization-provider-name]/[replication-domain-name]"
