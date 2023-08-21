cf07f23c09683eac27719ef69858fe7fe91a2c17
henryrecker-pingidentity <58565376+henryrecker-pingidentity@users.noreply.github.com>
2023-08-21 15:30:36 -0500

    Doc fixes (#286)
    * Adjust some examples and update supported PD versions
    
    * Fix getting-started example and add detail to doc for baseline schema
    
    * Add description of the type attribute on the index page
    
    * Reorder index doc
    
    * Generate docs

--- examples/resources/pingdirectory_plugin/resource.tf
+++ examples/resources/pingdirectory_plugin/resource.tf
@@ -1,4 +1,6 @@
-resource "pingdirectory_default_plugin" "myPlugin" {
-  name    = "MyPlugin"
-  enabled = false
+resource "pingdirectory_plugin" "myPlugin" {
+  name            = "MyPlugin"
+  resource_type   = "third-party"
+  enabled         = false
+  extension_class = "com.Example"
 }
