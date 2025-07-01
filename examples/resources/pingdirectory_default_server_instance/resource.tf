aba8f3a4fe244d148884c2611409d440373fd4d6
henryrecker-pingidentity <58565376+henryrecker-pingidentity@users.noreply.github.com>
2025-07-01 10:15:31 -0500

    Remove 9.3 testing and update doc version refs (#439)
    * Remove 9.3 testing and update doc version refs
    
    No 9.3 latest images are available on docker hub anymore
    
    * Use newer patches in tests

--- examples/resources/pingdirectory_default_server_instance/resource.tf
+++ examples/resources/pingdirectory_default_server_instance/resource.tf
@@ -1,5 +1,5 @@
 resource "pingdirectory_default_directory_server_instance" "myServerInstance" {
   name                 = "MyServerInstance"
   server_instance_name = "MyDirectoryServerInstance"
-  server_version       = "9.3.0.0"
+  server_version       = "10.2.0.0"
 }
